7512624038812b744d0283602fdb54f8b1dd5b8d
Louis-7 <louisgh.cn@gmail.com>
2018-08-15 13:38:07 +0800

    [update] copyright
    Signed-off-by: Louis-7 <louisgh.cn@gmail.com>

--- webClient/src/app/shared/dialog/open-dataset/open-dataset.component.spec.ts
+++ webClient/src/app/shared/dialog/open-dataset/open-dataset.component.spec.ts
@@ -1,3 +1,13 @@
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OpenDataSetComponent } from './open-dataset.component';
@@ -23,3 +33,13 @@ describe('OpenFolderComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
