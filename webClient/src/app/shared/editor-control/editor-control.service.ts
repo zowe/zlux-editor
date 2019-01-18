e4eccf67ea7d92920c63c318616db94caf64111e
Jordan Filteau <jfilteau@rocketsoftware.com>
2019-01-18 15:53:30 -0500

    copyrights

--- webClient/src/app/shared/editor-control/editor-control.service.ts
+++ webClient/src/app/shared/editor-control/editor-control.service.ts
@@ -3,6 +3,16 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
 
   SPDX-License-Identifier: EPL-2.0
 
@@ -807,5 +817,15 @@ export class EditorControlService implements ZLUX.IEditor, ZLUX.IEditorMultiBuff
 
   SPDX-License-Identifier: EPL-2.0
 
+  Copyright Contributors to the Zowe Project.
+*/
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
   Copyright Contributors to the Zowe Project.
 */
