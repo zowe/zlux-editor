da2b35e5a8859fe21594da3d98e8ec44eb78fa39
Nakul Manchanda <nakul.manchanda@ibm.com>
2019-05-14 17:22:11 -0400

    selecting first tab on close of selected tab
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- webClient/src/app/editor/code-editor/code-editor.component.ts
+++ webClient/src/app/editor/code-editor/code-editor.component.ts
@@ -61,6 +61,16 @@ export class CodeEditorComponent implements OnInit {
       list.length === 0 ? this.noOpenFile = true : this.noOpenFile = false;
     });
 
+    this.editorControl.closeFile.subscribe((fileContext: ProjectContext) => {
+      if (!this.noOpenFile && !this.isAnySelected()) {
+        this.selectFile(this.openFileList[0], true);
+      }
+    });
+
+  }
+
+  isAnySelected () {
+    return this.openFileList.some(f=>f.active)
   }
 
   ngOnInit() { }
@@ -106,8 +116,8 @@ export class CodeEditorComponent implements OnInit {
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
