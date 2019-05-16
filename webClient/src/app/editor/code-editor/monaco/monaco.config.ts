2e434642b263c571db1c032bbfda734ba7d82d6a
Leanid Astrakou <lastrakou@rocketsoftware.com>
2019-05-16 11:25:00 -0400

    Minor QA regex changes
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- webClient/src/app/editor/code-editor/monaco/monaco.config.ts
+++ webClient/src/app/editor/code-editor/monaco/monaco.config.ts
@@ -95,7 +95,7 @@ const JCL_HILITE = {
   tokenizer: {
     root: [
       [/, *$/, { token: 'default', next: '@operands2' }], //Checks for end of line with a ','
-      [/$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
+      [/ *\n| *$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
       [/<</, { token: 'jcl-delimiter', next: '@comments'}], //Checks for <<
       [/,( +)[0-9]+$/, { token: 'jcl-delimiter', next: '@operands2'}], //Checks for ',' + linenumber + linebreak (continuation of statement)
       [/( *)[0-9]+$/, { token: 'default' }], //Checks for linenumber + linebreak (new JCL statement)
@@ -108,7 +108,7 @@ const JCL_HILITE = {
     name: [
       
       [/, *$/, { token: 'default', next: '@operands2' }], //Checks for end of line with a ','
-      [/$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
+      [/ *\n| *$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
       [/,( +)[0-9]+$/, { token: 'jcl-delimiter', next: '@operands2'}], //Checks for ',' + linenumber + linebreak (continuation of statement)
       [/( *)[0-9]+$/, { token: 'default', next: '@popall' }], //Checks for linenumber + linebreak (new JCL statement)
       [/( +)/, { token: 'whitespace', next: '@operator' }], //Spaces(s) designate when to check for KEYWORDS after root
@@ -122,19 +122,21 @@ const JCL_HILITE = {
     operator: [
       //[/( +)/, { token: 'whitespace' }],
       [/, *$/, { token: 'default', next: '@operands2' }], //Checks for end of line with a ','
-      [/$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
+      [/ *\n| *$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
       [/!/, { token: 'jcl-invalid', next: '@operands' }], // Checks for invalid JCL characters
       [/[a-z]+/, { token: 'jcl-invalid', next: '@operands' }], // Checks for invalid lowercase JCL
       [/(,|&|=|\^|\(|\))/, { token: 'jcl-delimiter', next: '@operands'}],
       [/(IF)/, { token: 'jcl-operator', next: '@if' }],
+      [/(DD|CNTL|EXEC|JOB|INCLUDE|JCLLIB|OUTPUT|PROC|SCHEDULE|SET|XMIT|COMMAND) *$/, { token: 'jcl-operator', next: '@popall' }],
       [/(DD|CNTL|EXEC|JOB|INCLUDE|JCLLIB|OUTPUT|PROC|SCHEDULE|SET|XMIT|COMMAND) +/, { token: 'jcl-operator', next: '@operands' }],
+      [/(ENDCNTL|EXPORT|ELSE|ENDIF|PEND|THEN) *$/, { token: 'jcl-operator', next: '@popall' }],
       [/(ENDCNTL|EXPORT|ELSE|ENDIF|PEND|THEN) +/, { token: 'jcl-operator', next: '@comments' }],
       [/[^\s\\a-z]+/, { token: 'default', next: '@operands'}],
       //[/..../, { token: 'default', next: '@operands' }],
     ],
     if: [
       [/, *$/, { token: 'default', next: '@operands2' }], //Checks for end of line with a ','
-      [/$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
+      [/ *\n| *$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
       [/(THEN )/, { token: 'jcl-operator', next: '@comments' }],
       [/./, { token: 'jcl-variable' }],
     ],
@@ -148,7 +150,7 @@ const JCL_HILITE = {
       [/,( +)[0-9]+$/, { token: 'jcl-delimiter', next: '@operands2'}], //Checks for ',' + linenumber + linebreak (continuation of statement)
       [/( *)[0-9]+$/, { token: 'default', next: '@popall' }], //Checks for linenumber + linebreak (new JCL statement)
       [/, *$/, { token: 'default', next: '@operands2' }], //Checks for end of line with a ','
-      [/$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
+      [/ *\n| *$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
       [/, /, { token: 'jcl-delimiter', next: '@comments' }], //Checks for , + space (leads to comment)
       [/'/, { token: 'jcl-string', next: '@strings' }],
       [/!/, { token: 'jcl-invalid' }], // Checks for invalid JCL characters
@@ -164,7 +166,7 @@ const JCL_HILITE = {
     operands2: [ //JCL has a behavior where it will accept two sets of operands before detecting comments
                  //for certain conditions, usually when statements are continued via a ','
       [/, *$/, { token: 'default', next: '@operands2' }], //Checks for end of line with a ','
-      [/$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
+      [/ *\n| *$/, { token: 'default', next: '@popall' }], //Checks for end of line without a ','
       [/,( +)[0-9]+$/, { token: 'jcl-delimiter', next: '@operands2'}], //Checks for ',' + linenumber + linebreak (continuation of statement)
       [/( *)[0-9]+$/, { token: 'default', next: '@popall' }], //Checks for linenumber + linebreak (new JCL statement)
       [/, /, { token: 'jcl-delimiter', next: '@comments' }], //Checks for , + space (leads to comment)
@@ -181,7 +183,7 @@ const JCL_HILITE = {
     ],
     comments: [
       [/.*/, { token: 'jcl-comment-//*-all', next: '@popall' }],
-      [/$/, { token: 'default', next: '@popall' }],
+      [/ *\n| *$/, { token: 'default', next: '@popall' }],
     ],
     strings: [ //Strings get their own category because Monaco doesn't seem to deal with pattern matching
               //over line breaks, even with multiline flags. This way, we just put strings into their own loop.
