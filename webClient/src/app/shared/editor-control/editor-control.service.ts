32df594eb647650a3c30b88dc3d7aef1f885a47a
Leanid Astrakou <lastrakou@rocketsoftware.com>
2019-12-09 17:14:19 -0500

    Removed PDSE from write support check
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- webClient/src/app/shared/editor-control/editor-control.service.ts
+++ webClient/src/app/shared/editor-control/editor-control.service.ts
@@ -28,6 +28,8 @@ import { MessageDuration } from "../message-duration";
 
 let stateCache = {};
 let lastFile;
+//Unsupported DS types
+const unsupportedTypes: Array<string> = ['G', 'B', 'C', 'D', 'I', 'R'];
 
 export let EditorServiceInstance: BehaviorSubject<any> = new BehaviorSubject(undefined);
 /**
@@ -267,17 +269,18 @@ export class EditorControlService implements ZLUX.IEditor, ZLUX.IEditorMultiBuff
     // G - Alternate index
     // I - VSAM Index
     // R - VSAM Path
-    const unsupportedTypes: Array<string> = ['G', 'B', 'C', 'D', 'I', 'R'];
+    
     if (model) {
       if (model.datasetAttrs) {
         //VSAM & GDG are currently not supported for write mode
         if (unsupportedTypes.includes(model.datasetAttrs.csiEntryType)) {
           editor.updateOptions({ readOnly: true });
         }
+        // TODO: Uncomment this in case PDSE is not supported for Dataset writing
         //PDSE is not supported for write mode
-        if (model.datasetAttrs.dsorg.isPDSE && model.datasetAttrs.dsorg.isPDSE == true) {
-          editor.updateOptions({ readOnly: true });
-        }
+        // if (model.datasetAttrs.dsorg.isPDSE && model.datasetAttrs.dsorg.isPDSE == true) {
+        //   editor.updateOptions({ readOnly: true });
+        // }
       }
     }
   }
