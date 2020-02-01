09dd6c3800cb10271dc0cda56752901b25ba2f75
pranav1698 <hulkcodexl16@gmail.com>
2020-02-02 00:21:49 +0530

    Adding minified notification for the open directory option
    Signed-off-by: pranav1698 <hulkcodexl16@gmail.com>

--- webClient/src/app/shared/dialog/open-folder/open-folder.component.ts
+++ webClient/src/app/shared/dialog/open-folder/open-folder.component.ts
@@ -3,15 +3,16 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { HttpService } from '../../http/http.service';
 import { ENDPOINTS } from '../../../../environments/environment';
+import { SnackBarService } from '../../snack-bar.service';
 
 @Component({
   selector: 'app-open-folder',
@@ -23,9 +24,15 @@ export class OpenFolderComponent implements OnInit {
   private fetching = false;
   private value = '/';
 
-  constructor(private http: HttpService, private dialogRef: MatDialogRef<OpenFolderComponent>) { }
+  constructor(private http: HttpService, private dialogRef: MatDialogRef<OpenFolderComponent>, private snackBar: SnackBarService) { }
 
   ngOnInit() {
+  }
+
+  openFile(){
+      this.snackBar.open("File Not Found!","", {
+          duration: 2000,
+      });
   }
 }
 
@@ -33,8 +40,8 @@ export class OpenFolderComponent implements OnInit {
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
