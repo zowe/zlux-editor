128f0edd28a9d748a26b1675bbff0f238862ad80
Leanid Astrakou <LeanyIA@yahoo.com>
2023-01-17 15:22:42 -0500

    Fix a bug with DS open not working
    Signed-off-by: Leanid Astrakou <LeanyIA@yahoo.com>

--- webClient/src/app/editor/project-tree/project-tree.component.ts
+++ webClient/src/app/editor/project-tree/project-tree.component.ts
@@ -165,7 +165,7 @@ export class ProjectTreeComponent {
           let dsMemberName;
           if (dirName == dsName) {
             let periodPos = dirName.lastIndexOf('.');
-            if (periodPos) {
+            if (periodPos >= 0) {
               this.fileExplorer.updateDSList(dirName.substring(0,periodPos+1)+'**');
             } else {
               this.fileExplorer.updateDSList(dirName);
