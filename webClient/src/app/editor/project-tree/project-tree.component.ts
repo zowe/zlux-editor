b393758a8aeab6d7b88f3335998341007ee8e653
Leanid Astrakou <lastrakou@rocketsoftware.com>
2025-07-23 21:18:16 -0400

    Fixed MVS file not opening
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- webClient/src/app/editor/project-tree/project-tree.component.ts
+++ webClient/src/app/editor/project-tree/project-tree.component.ts
@@ -22,7 +22,7 @@ import { UtilsService } from '../../shared/utils.service';
 import { DataAdapterService } from '../../shared/http/http.data.adapter.service';
 import { SnackBarService } from '../../shared/snack-bar.service';
 import { Angular2InjectionTokens } from 'pluginlib/inject-resources';
-import { ZluxFileTreeComponent } from 'zlux-angular-file-tree';
+import { ZluxFileTreeComponent } from '@zowe/zlux-angular-file-tree';
 
 @Component({
   selector: 'app-project-tree',
